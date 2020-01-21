e97b10c06fc7f485cdb68c6cf758644d32f77ea0
diegopgcabral <diegopgc@gmail.com>
2020-01-21 14:57:01 -0300

    first commit

---
+++ web/src/App.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+import DevItem from '../src/components/DevItem';
+import DevForm from '../src/components/DevForm';
+
+function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function HandleAddDev(data) {
+    console.log(data);
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={HandleAddDev} />
+      </aside>
+
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
